e80f59a92082bc4a50e58045213ec3adda9d186e
David Jay <davidgljay@gmail.com>
2022-09-20 21:57:00 -0700

    Refining script.

--- index.js
+++ index.js
@@ -1,54 +1,136 @@
 
+async function run() {
+
 // Set Constants
 
-const notionDb = 'ADD_ME'
-const notionToken = 'ADD_ME'
-const eventUrlField = ''
+const notionToken = ''
+
+const contactDb = ''
+const eventDb = ''
+const orgDb = ''
+const registrationDb = ''
+const eventIdField = ''
 const orgTitleField = ''
 const contactNameField = ''
 const contactEmailField = ''
 const contactOrgField = ''
+const contactTitleField = ''
 const registrationContactField = ''
 const registrationEventField = ''
+const registrationStatusField = ''
 
 
 // Gather inputData
 
-// Set Params
+const inputData = {
+  name: 'David Jay',
+  email: 'davidgljay@gmail.com',
+  hasJoined: 'false',
+  lumaEventId: 'evt-PhSMp6DjpNLkEk1',
+  answers: 'Relationality Lab, Founder'
+}
+
+
+const {name, lumaEventId, email, answers, hasJoined} = inputData
+
+const org = answers.split(',')[0]
+const title = answers.split(',')[1]
+
+// Define functions used above
+
+const errorHandler = err => {
+  return err.text().then(text => new Error(err.status + ': ' + err.statusText + "   " + text))
+}
+
+
+const notionFind = (db, filter) => {
+  const url = 'https://api.notion.com/v1/databases/' + db + '/query'
+  const params = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Notion-Version': '2022-06-28',
+      'Authorization': 'Bearer ' + notionToken
+    },
+    body: JSON.stringify({filter})
+  }
+  return fetch(url, params)
+    .then(res => res.ok ? res.json() : errorHandler(res))
+    .then(json => json.results)
+
+}
+
+const notionPost = (db, properties) => {
+  const url = 'https://api.notion.com/v1/pages'
+  const body = {
+    parent: { type: "database_id", database_id: db },
+    properties,
+  }
+  const params = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Notion-Version': '2022-06-28',
+      'Authorization': 'Bearer ' + notionToken
+    },
+    body: JSON.stringify(body)
+  }
+  return fetch(url, params)
+    .then(res => res.ok ? res.json() : errorHandler(res))
+}
+
+const notionFindOrCreate = (db, filter, properties) => {
+  return notionFind(db, filter)
+    .then(results => results.length > 0 ? results : notionPost(db, properties).then(json => [json]) )
+ }
+
+ const notionUpdate = (id, properties) => {
+   const url = 'https://api.notion.com/v1/pages/' + id
+   const params = {
+     method: 'PATCH',
+     headers: {
+       'Content-Type': 'application/json',
+       'Notion-Version': '2022-06-28',
+       'Authorization': 'Bearer ' + notionToken
+     },
+     body: JSON.stringify({properties})
+   }
+   return fetch(url, params)
+     .then(res => res.ok ? res.json() : errorHandler(res))
+ }
 
 // Find Event
 
 const eventFilter = {
   and: [
     {
-      property: eventUrlField
-      url: {
-        equals: registrationUrl
+      property: eventIdField,
+      rich_text: {
+        contains: lumaEventId
       }
     }
   ]
 }
 
-const eventId = await notionFind(eventDB, eventFilter).then(results => results[0].id)
+const eventId = await notionFind(eventDb, eventFilter).then(results => results[0].id)
 
 // Find Org
 
 const orgFilter = {
   and: [
     {
-      property: orgTitleField
+      property: orgTitleField,
       title: {
-        equals: registrationOrg
+        equals: org
       }
     }
   ]
 }
 
 const orgProperties = {
-  {
     [orgTitleField]: {
       title: [{
-          text: { content: registrationOrg}
+          text: { content: org}
       }]
     }
 }
@@ -58,9 +140,9 @@ const orgId = await notionFindOrCreate(orgDb, orgFilter, orgProperties).then(res
 const contactFilter = {
   and: [
     {
-      property: contactEmailField
+      property: contactEmailField,
       email: {
-        equals: registrationEmail
+        equals: email
       }
     }
   ]
@@ -69,26 +151,29 @@ const contactFilter = {
 //Create Contact
 
 const contactProperties = {
-  {
     [contactNameField]: {
       title: [{
-          text: { content: registrationName}
+          text: { content: name}
       }]
     },
     [contactEmailField]: {
       email: [{
-          text: { content: registrationEmail}
+          text: { content: email}
       }]
     },
     [contactOrgField]: {
       relation: [{
         id: orgId
       }]
+    },
+    [contactTitleField]: {
+      rich_text: [{
+        text: { content: title}
+      }]
     }
-  }
 }
 
-const contactId = await notionFindorCreate(contactDB, contactFilter, contactProperties)
+const contactId = await notionFindOrCreate(contactDb, contactFilter, contactProperties).then(results => results[0].id)
 
 // Find or create registration
 
@@ -122,55 +207,26 @@ const registrationProperties = {
   }
 }
 
-const registrationid = await notionFindorCreate(registrationDb, registrationFilter, registrationProperties).then(results => results[0].id)
+const registrationId = await notionFindOrCreate(registrationDb, registrationFilter, registrationProperties).then(results => results[0].id)
+
+// Update registration
+
+const registrationUpdateProperties = {
+  [registrationStatusField]: {
+    select: {
+          name: hasJoined ? "Attended" : "Registered"
+      }
+    }
+}
+
+await notionUpdate(registrationId, registrationUpdateProperties)
 
 // Output
 
-outputData = {registrationId}
+output = {registrationId}
 
-// Define functions used above
 
-const errorHandler = err => {
-  return err.text().then(text => new Error(err.status + ': ' + err.statusText + "   " + text))
+
 }
 
-
-const notionFind = (db, filter) => {
-  const url = 'https://api.notion.com/v1/databases/' + db + '/query'
-  const params = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Notion-Version': '2022-06-28',
-      'Authorization': 'Bearer ' + notionToken
-    },
-    body: JSON.stringify({filter})
-  }
-  return fetch(url, params)
-    .then(res => res.ok ? res.json() : errorHandler(res))
-    .then(json => json.results)
-}
-
-const notionPost = (db, properties) => {
-  const url = 'https://api.notion.com/v1/pages'
-  const body = {
-    parent: { type: "database_id", database_id: notionDb },
-    properties,
-  }
-  const params = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Notion-Version': '2022-06-28',
-      'Authorization': 'Bearer ' + notionToken
-    },
-    body: JSON.stringify(body)
-  }
-  return fetch(url, params)
-    .then(res => res.ok ? res.json() : errorHandler(res))
-}
-
-const notionFindOrCreate = (db, filter, properties) => {
-  return notionFind(db, filter)
-    .then(results => results.length > 0 ? results : notionPost(db, properties).then(json => [json]) )
- }
+run()
